24153e3af4e7458a5c49a3dbcb8cb3952b190e86
Navidhashemicodes <navidhashemi.codes@gmail.com>
2025-04-20 09:04:53 -0500

    Updated files and added new benchmarks

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -8,70 +8,12 @@ import time
 import sys
 import pathlib
 from networks.neural_net_generator import generate_network
-
-RP_dir = str (pathlib.Path().resolve())
-sys.path.append( RP_dir )
-
 from formula_factory import FormulaFactory
 
-FOLDERNAME = RP_dir
-
-
-
 ###################################Accurate
-
-class RNetwork_acc(nn.Module):
-    def __init__(self, weights_file, biases_file):
-        super(RNetwork_acc, self).__init__()
-
-        # Load weights and biases from MATLAB files
-        weights_Linear_mat = loadmat(weights_file)['WL']
-        biases_Linear_mat = loadmat(biases_file)['BL']
-        weights_nLinear_mat = loadmat(weights_file)['Wn']
-        biases_nLinear_mat = loadmat(biases_file)['Bn']
-
-        # Convert cell array elements to PyTorch tensors with type conversion
-        self.weights_L = [torch.from_numpy(wi.astype(np.float32)) for wi in weights_Linear_mat[0]]
-        self.weights_n = [torch.from_numpy(wi.astype(np.float32)) for wi in weights_nLinear_mat[0]]
-        self.biases_L = [torch.from_numpy(bi.astype(np.float32)) for bi in biases_Linear_mat[0]]
-        self.biases_n = [torch.from_numpy(bi.astype(np.float32)) for bi in biases_nLinear_mat[0]]
-
-        # Calculate the number of hidden layers based on the length of weights
-        num_hidden_layers = len(self.weights_L) - 1
-
-        # Define the hidden layers using predefined weights and biases
-        self.hidden_layers = nn.ModuleList([
-            nn.Linear( (self.weights_n[i]).shape[1], (self.weights_n[i]).shape[0])
-            for i in range(num_hidden_layers)
-        ])
-
-        # Output layer
-        self.output_layer = nn.Linear((self.weights_L[-1]).shape[1], (self.weights_L[-1]).shape[0])
-        
-        
-        for i, layer in enumerate(self.hidden_layers):
-            layer.weight.data = self.weights_n[i]  # Transpose for proper shape
-            layer.bias.data = self.biases_n[i]
-
-        self.output_layer.weight.data = self.weights_L[-1]
-        self.output_layer.bias.data = self.biases_L[-1]
-
-    def forward(self, x):
-        # Forward pass through hidden layers with ReLU activation
-        for i, layer in enumerate(self.hidden_layers):
-            xn = nn.functional.relu(layer(x))
-            xl = torch.matmul( x , (self.weights_L[i]).t()  ) + self.biases_L[i]
-            x=[]
-            x.append(xl)
-            x.append(xn)
-            x = torch.cat(x , dim=1) 
-            
-        # Output layer
-        x = self.output_layer(x)
-        return x
-
 T = 40
 Batch = 1
+Epochs = 100
 device = torch.device("cpu")
 args = {'T': T+1, 'd_state': 3, 'Batch': Batch, 'approximation_beta': 1, 'device': device, 'detailed_str_mode': False}
 FF = FormulaFactory(args)
@@ -149,29 +91,14 @@ SFE = FF.G(  or1,   0,   T  )
 
 my_formula  =  FF.And( [ ordered , SFE ] )
 
-
-
-
-weights_file_path_acc = FOLDERNAME + '/weights/weights_acc.mat'
-biases_file_path_acc = FOLDERNAME + '/weights/biases_acc.mat'
-
-r_network_acc = RNetwork_acc(weights_file_path_acc, biases_file_path_acc)
-neural_net = generate_network(my_formula, approximate=True, beta=10).to(args['device'])
-    
-
+neural_net = generate_network(my_formula, approximate=False, beta=10).to(args['device'])
 start_time =time.time()
-for i in range(10):
-    
-    
+
+for i in range(Epochs):
     trajectory = torch.randn( Batch, T+1, 3 ).to(device)
-    # objective_value1 =  my_formula.evaluate(trajectory)[0]
     objective_value1 = neural_net(trajectory)
-    print(objective_value1)
-    trajectory_check =  trajectory.reshape(1,-1)
-    objective_value2 = r_network_acc(trajectory_check)
-    print(objective_value2)
-    print('#############')
+    # trajectory_check =  trajectory.reshape(1,-1)
 
 end_time = time.time()
 
-print(end_time-start_time)
+print("Time:", end_time - start_time)
